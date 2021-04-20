fa428b9e8993c1d83fab2edffa867a6293d2d98e
Almeida <lalmeida6@PMINTL.NET>
2021-04-20 01:02:23 -0300

    commit inicial

---
+++ lib/widgets/sub_result.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class SubResult extends StatelessWidget {
+  final String text;
+
+  const SubResult({
+    Key key,
+    this.text,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      alignment: Alignment.centerRight,
+      child: Text(
+        this.text,
+        style: TextStyle(
+          fontSize: 30,
+        ),
+      ),
+    );
+  }
+}
